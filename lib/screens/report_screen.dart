223ea48cd041beacea9d98c7c4e907e18010950e
cepot-blip <85933775+cepot-blip@users.noreply.github.com>
2023-10-19 11:05:59 +0700

    fix error

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ReportScreen extends StatefulWidget {
-  const ReportScreen({Key? key}) : super(key: key);
+class ReportsScreen extends StatefulWidget {
+  const ReportsScreen({Key? key}) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
   _ReportScreenState createState() => _ReportScreenState();
 }
 
-class _ReportScreenState extends State<ReportScreen> {
+class _ReportScreenState extends State<ReportsScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
